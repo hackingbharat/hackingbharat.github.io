2b790a9cb42c7ab792eef1b6b37d572bbadac231
hackingbharat <53665211+hackingbharat@users.noreply.github.com>
2020-08-06 18:25:23 +0530

    Delete ip.js

--- ip.js
+++
@@ -1,7 +0,0 @@
-var request = new XMLHttpRequest();
-request.open("GET", "http://ipinfo.io/json", true);
-request.send();
-request.onreadystatechange = function() {
-    if (request.readyState == 4)
-	      alert(request.responseText);
-}
